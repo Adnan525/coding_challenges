70345677985f9ce89d11b3f876767c59e7ad0a38
Adnan525 <adnaan525@gmail.com>
2021-09-19 11:51:10 +1000

    IPv4 regex from hacker rank

---
+++ src/IPv4Regex/MyRegex.java
@@ -0,0 +1,22 @@
+package IPv4Regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+class MyRegex {
+    String pattern= "((0{1,3}|0{0,2}[1-9]|0?[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}(0{1,3}|0{0,2}[1-9]|0?[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$";
+}
