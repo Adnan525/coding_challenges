18919e3d75c5e18081e358fac8569fc4755ed037
Adnan525 <adnaan525@gmail.com>
2022-06-16 01:02:13 +1000

    User name validator regex hacker rank

---
+++ src/UserNameValidator/UsernameValidator.java
@@ -0,0 +1,24 @@
+package UserNameValidator;
+import java.util.Scanner;
+
+class UsernameValidator {
+    public static final String regularExpression = "^([A-Z]|[a-z]){1}\\w{7,29}";
+}
+
+
+class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
