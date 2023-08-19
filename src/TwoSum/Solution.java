9cc30036af245a3a5df807bb15d78896a1e96aac
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-08-19 16:21:27 +0000

    two sum leetcode faster

--- src/TwoSum/Solution.java
+++ src/TwoSum/Solution.java
@@ -7,7 +7,7 @@ public class Solution{
     {
         for(int i = 0; i<arr.length; i++)
         {
-            for(int j = 0; j<arr.length; j++)
+            for(int j = i+1; j<arr.length; j++)
             {
                 if(arr[i] + arr[j] == target){
                     if(i != j)
