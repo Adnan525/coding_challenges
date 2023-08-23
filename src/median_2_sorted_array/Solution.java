740598cd1182d6219ea4578f37814d8bdc96ee6d
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-08-23 13:52:18 +0000

    median of 2 sorted array meta data

--- src/median_2_sorted_array/Solution.java
+++ src/median_2_sorted_array/Solution.java
@@ -4,8 +4,13 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // stream of int[] arr, boxed makes primitive int to Integer, converts to Integer[]
+
         Integer[] arr1 = Arrays.stream(nums1).boxed().toArray( Integer[]::new );
         Integer[] arr2 = IntStream.of(nums2).boxed().toArray( Integer[]::new );
+
+        //flatMap operation is used to transform each element of the stream into another stream and then flatten the resulting streams into a single stream.
+
         Object[] merged = Stream.of(arr1, arr2).flatMap(Stream::of).toArray();
         Arrays.sort(merged);
         return (merged.length % 2 == 0) ? //even
