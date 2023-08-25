9937b16ff80b0db02bfb199c329f91c45b68587e
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-08-25 03:35:31 +0000

    most water container - time limit exceeded

---
+++ src/most_water_container/Solution.java
@@ -0,0 +1,17 @@
+package most_water_container;
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for(int i = 0; i<height.length; i++)
+        {
+            int tempX = height[i];
+            for(int j = i + 1; j<height.length; j++)
+            {
+                int area = (tempX < height[j]) ? (j-i) * tempX : (j-i) * height[j];
+                if (area > maxArea)
+                    maxArea = area;
+            }
+        }
+        return maxArea;
+    }
+}
