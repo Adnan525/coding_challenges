4e762a12a4139353166ee4ac3cd7cbe1f0718794
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-08-25 03:46:38 +0000

    wrong answer e49, o24

--- src/most_water_container/Solution.java
+++ src/most_water_container/Solution.java
@@ -1,6 +1,23 @@
 package most_water_container;
 class Solution {
+
     public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length -1;
+        int maxArea = 0;
+        while(i !=j)
+        {
+            int area = (height[i] < height[j]) ? height[i] * (j - i) : height[j] * (j - i);
+            if(area>maxArea)
+                maxArea = area;
+            i++;
+            j--;
+        }
+        return maxArea;
+        
+    }
+
+    public int test(int[] height) {
         int maxArea = 0;
         for(int i = 0; i<height.length; i++)
         {
