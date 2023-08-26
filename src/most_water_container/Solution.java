59257d08407c48088063c7b343ea69f23bdcf069
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2023-08-26 16:17:29 +0000

    max container done - see explaination in class

--- src/most_water_container/Solution.java
+++ src/most_water_container/Solution.java
@@ -1,17 +1,23 @@
 package most_water_container;
 class Solution {
 
+    // the idea is we start with highest length so either side of the array
+    // move inwards
+    //but move inwards only for the less height
+    //so basically, the highest value of the array will definitely be one of the 2 sides
     public int maxArea(int[] height) {
+        int start = 0;
+        int highestLength = height.length-1;
         int maxArea = 0;
-        for(int i = 0; i<height.length; i++)
+        while(start != highestLength)
         {
-            int tempX = height[i];
-            for(int j = height.length -1; j>i; j--)
-            {
-                int area = (tempX < height[j]) ? (j-i) * tempX : (j-i) * height[j];
-                if (area > maxArea)
-                    maxArea = area;
-            }
+            int tempArea = (height[start] < height[highestLength]) ? height[start] * (highestLength - start) : height[highestLength] * (highestLength - start);
+            if(tempArea>maxArea)
+                maxArea = tempArea;
+            if(height[start] < height[highestLength])
+                start++;
+            else
+                highestLength--;
         }
         return maxArea;
         
