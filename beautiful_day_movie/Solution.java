ae181f46a310f1349061e600e4e4231f01d9a962
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2024-01-08 14:20:05 +0000

    beautiful day movie passed

---
+++ beautiful_day_movie/Solution.java
@@ -0,0 +1,64 @@
+package beautiful_day_movie;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k) {
+        return (int) IntStream.rangeClosed(i, j)
+                    .filter(val -> is_beautiful(val, k))
+                    .count();
+    }
+
+    private static boolean is_beautiful(int num, int k){
+        String temp = String.valueOf(num);
+        String rev_temp = new StringBuilder(temp).reverse().toString();
+        Integer rev_num = Integer.parseInt(rev_temp);
+        return Math.abs(rev_num - num) % k == 0 ? true : false;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
