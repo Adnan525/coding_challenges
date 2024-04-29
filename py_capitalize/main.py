02221dc23da6dc97d6514b453a107bca9aceff76
Muntasir Adnan <48549305+Adnan525@users.noreply.github.com>
2024-04-29 15:33:26 +0000

    python minion game done

--- py_capitalize/main.py
+++ py_capitalize/main.py
@@ -6,18 +6,23 @@ import sys
 
 
 def solve(s):
-    word_pattern = r"\b[a-z]+\b"
+    temp = s
+    word_pattern = r"\b\w+\b"
     words = re.findall(word_pattern, s)
-    print(" ".join([word.capitalize() for word in words]))
-    return [word.capitalize() for word in words]
+    for word in words:
+        temp = temp.replace(word, word.capitalize())
+    # print(" ".join([word.capitalize() for word in words]))
+    print(temp)
+    # return [word.capitalize() for word in words]
+    return temp
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    s = "muntasir adnan"
+    s = "q w e r t y u i o p a s d f g h j  k l z x c v b n m Q W E R T Y U I O P A S D F G H J  K L Z X C V B N M"
 
     result = solve(s)
-    print(result)
+    # print(result)
 
     # fptr.write(result + '\n')
 
